6f3ca9c3b4593c9eff9aba4f5bb14e1c08018a15
iris dev <iris-engine-dev@gmail.com>
2022-11-04 19:48:53 +0000

    Refactor window code

---
+++ asm/graphics.asm
@@ -0,0 +1,226 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;         Distributed under the Boost Software License, Version 1.0.         ;;
+;;            (See accompanying file LICENSE or copy at                       ;;
+;;                 https://www.boost.org/LICENSE_1_0.txt)                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+global create_window
+global try_get_event
+
+extern XBlackPixel
+extern XCheckWindowEvent
+extern XClearWindow
+extern XCreateGC
+extern XCreateSimpleWindow
+extern XDefaultRootWindow
+extern XDefaultScreen
+extern XFillRectangle
+extern XFlush
+extern XLookupKeysym
+extern XMapWindow
+extern XNextEvent
+extern XSync
+extern XOpenDisplay
+extern XSelectInput
+extern XSetForeground
+extern XWhitePixel
+
+extern assert_not_null
+extern assert_null
+extern render_begin
+extern draw_rectangle
+extern render_end
+extern print
+extern print_num
+
+; create an X Window
+create_window:
+    push rbp
+    mov rbp, rsp
+
+    ; X11 window setup
+
+    mov rdi, 0x0
+    call XOpenDisplay
+    mov [display], rax
+    mov rdi, rax
+    lea rsi, [x_open_display_failed]
+    call assert_not_null
+
+    call XDefaultScreen
+    mov [screen_number], rax
+    mov rdi, rax
+    call print_num
+
+    mov rdi, [display]
+    mov rsi, [screen_number]
+    call XWhitePixel
+    mov [white_colour], rax
+
+    mov rdi, [display]
+    mov rsi, [screen_number]
+    call XBlackPixel
+    mov [black_colour], rax
+
+    mov rdi, [display]
+    call XDefaultRootWindow
+    mov [default_root_window], rax
+
+    mov rdi, [display]
+    mov rsi, [default_root_window]
+    mov rdx, 0x0
+    mov rcx, 0x0
+    mov r8, 0x320
+    mov r9, 0x320
+    mov rax, [black_colour]
+    push rax
+    push rax
+    push 0x0
+    call XCreateSimpleWindow
+    mov [window], rax
+    add rsp, 0x18
+
+    mov rdi, [display]
+    mov rsi, [window]
+    mov rdx, 0x20003
+    call XSelectInput
+
+    mov rdi, [display]
+    mov rsi, [window]
+    call XMapWindow
+
+    mov rdi, [display]
+    mov rsi, [window]
+    mov rdx, 0x0
+    mov rcx, 0x0
+    call XCreateGC
+    mov [gc], rax
+
+    ; wait until window has appeared
+wait_loop_start:
+    mov rdi, [display]
+    lea rsi, [event]
+    call XNextEvent
+
+    mov eax, [event]
+    cmp rax, 0x13
+    je wait_loop_end
+
+    jmp wait_loop_start
+wait_loop_end:
+
+    pop rbp
+    ret
+
+; Try and get an event
+; returns
+;   A pointer to an event, or 0x0 if no event was available
+try_get_event:
+    push rbp
+    mov rbp, rsp
+
+    ; see if we want have events
+    mov rdi, [display]
+    mov rsi, [window]
+    mov rdx, 0x03
+    lea rcx, [event]
+    call XCheckWindowEvent
+
+    cmp rax, 0x0
+    je try_get_event_end
+
+    ; we got an event so return pointer to event object
+    lea rax, [event]
+
+try_get_event_end:
+    pop rbp
+    ret
+
+; Perform pre-render tasks
+render_begin:
+    push rbp
+    mov rbp, rsp
+
+    ; clear window
+    mov rdi, [display]
+    mov rsi, [window]
+    call XClearWindow
+
+    pop rbp
+    ret
+
+; Draw a rectangle
+; @param rdi
+;   X coord of rectangle
+;
+; @param rsi
+;   Y coord of rectangle
+;
+; @param rdx
+;   Width of rectangle
+;
+; @param rcx
+;   Height of rectangle
+draw_rectangle:
+    push rbp
+    mov rbp, rsp
+
+    ; push args to stack so we can easily pop them into the correct registers
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+
+    ; set foreground color for drawing
+    mov rdi, [display]
+    mov rsi, [gc]
+    mov rdx, [white_colour]
+    call XSetForeground
+
+    ; draw paddle
+    mov rdi, [display]
+    mov rsi, [window]
+    mov rdx, [gc]
+    pop rcx
+    pop r8
+    pop r9 ; note that the last arg is now at at the top of the stack
+    call XFillRectangle
+    add rsp, 0x8
+
+    pop rbp
+    ret
+
+; Perform post-render tasks
+render_end:
+    push rbp
+    mov rbp, rsp
+
+    ; ensure all draw commands are flushed
+    mov rdi, [display]
+    call XFlush
+
+    ; sync commands with server
+    mov rdi, [display]
+    mov rsi, 0
+    call XSync
+
+    pop rbp
+    ret
+
+section .data
+    display: dq 0x0
+    screen_number: dq 0x0
+    black_colour: dq 0x0
+    white_colour: dq 0x0
+    default_root_window: dq 0x0
+    window: dq 0x0
+    gc: dq 0x0
+    event: resb 0xc0
+
+section .rodata
+    hello_world: db "hello world", 0xa, 0x0
+    goodbye: db "goodbye", 0xa, 0x0
+    sleep_for: db "sleep_for: ", 0x0
+    x_open_display_failed: db "XOpenDisplay failed", 0xa, 0x0
+    x_select_input_failed: db "XSelectInput failed", 0xa, 0x0
+    x_set_foreground_failed: db "XSetForeground failed", 0xa, 0x0
--- asm/main.asm
+++ asm/main.asm
@@ -9,25 +9,21 @@ section .text
 global _start
 
 extern XBlackPixel
-extern XCheckWindowEvent
 extern XClearWindow
-extern XCreateGC
-extern XCreateSimpleWindow
-extern XDefaultRootWindow
-extern XDefaultScreen
 extern XFillRectangle
 extern XFlush
 extern XLookupKeysym
-extern XMapWindow
-extern XNextEvent
 extern XSync
-extern XOpenDisplay
-extern XSelectInput
 extern XSetForeground
 extern XWhitePixel
 
 extern assert_not_null
 extern assert_null
+extern create_window
+extern try_get_event
+extern render_begin
+extern render_end
+extern draw_rectangle
 extern exit
 extern get_time
 extern print
@@ -39,106 +35,33 @@ _start:
     lea rdi, [hello_world]
     call print
 
-    ; X11 window setup
-
-    mov rdi, 0x0
-    call XOpenDisplay
-    mov [display], rax
-    mov rdi, rax
-    lea rsi, [x_open_display_failed]
-    call assert_not_null
-
-    call XDefaultScreen
-    mov [screen_number], rax
-    mov rdi, rax
-    call print_num
-
-    mov rdi, [display]
-    mov rsi, [screen_number]
-    call XWhitePixel
-    mov [white_colour], rax
-
-    mov rdi, [display]
-    mov rsi, [screen_number]
-    call XBlackPixel
-    mov [black_colour], rax
-
-    mov rdi, [display]
-    call XDefaultRootWindow
-    mov [default_root_window], rax
-
-    mov rdi, [display]
-    mov rsi, [default_root_window]
-    mov rdx, 0x0
-    mov rcx, 0x0
-    mov r8, 0x320
-    mov r9, 0x320
-    mov rax, [black_colour]
-    push rax
-    push rax
-    push 0x0
-    call XCreateSimpleWindow
-    mov [window], rax
-    add rsp, 0x18
-
-    mov rdi, [display]
-    mov rsi, [window]
-    mov rdx, 0x20003
-    call XSelectInput
-
-    mov rdi, [display]
-    mov rsi, [window]
-    call XMapWindow
-
-    mov rdi, [display]
-    mov rsi, [window]
-    mov rdx, 0x0
-    mov rcx, 0x0
-    call XCreateGC
-    mov [gc], rax
-
-    ; wait until window has appeared
-wait_loop_start:
-    mov rdi, [display]
-    lea rsi, [event]
-    call XNextEvent
-
-    mov eax, [event]
-    cmp rax, 0x13
-    je wait_loop_end
-
-    jmp wait_loop_start
-wait_loop_end:
+    call create_window
 
 main_loop_start:
     ; get time at start of frame
     call get_time
     mov [frame_time], rax
 
-    ; see if we want have events
-    mov rdi, [display]
-    mov rsi, [window]
-    mov rdx, 0x03
-    lea rcx, [event]
-    call XCheckWindowEvent
+    call try_get_event
+    mov r12, rax
 
     ; no event so go to game logic
     cmp rax, 0x0
     je game_logic
 
     ; check if its a press event
-    mov eax, [event]
+    mov eax, [r12]
     cmp rax, 0x2
     je handle_key
 
     ; check if its a release event
-    mov eax, [event]
+    mov eax, [r12]
     cmp rax, 0x3
     jne game_logic ; if not go to game logic
 
 handle_key:
     ; get the key code for the release event
-    lea rdi, [event]
+    lea rdi, [r12]
     mov rsi, 0x0
     call XLookupKeysym
 
@@ -147,7 +70,7 @@ handle_key:
     jne handle_arrow_key
 
     ; if its key press then exit the game
-    mov eax, [event]
+    mov eax, [r12]
     cmp rax, 0x2
     je main_loop_end
 
@@ -156,7 +79,7 @@ handle_arrow_key:
     cmp rax, 0xff53
     jne left_check
 
-    mov eax, [event]
+    mov eax, [r12]
     cmp rax, 0x2
     jne right_release
 
@@ -174,7 +97,7 @@ left_check:
     cmp rax, 0xff51
     jne game_logic
 
-    mov eax, [event]
+    mov eax, [r12]
     cmp rax, 0x2
     jne left_release
 
@@ -205,41 +128,19 @@ right_arrow_update_finish:
     mov [paddle_x], rax
 left_arrow_update_finish:
 
-    ; clear window
-    mov rdi, [display]
-    mov rsi, [window]
-    call XClearWindow
+    call render_begin
 
-    ; set foreground color for drawing
-    mov rdi, [display]
-    mov rsi, [gc]
-    mov rdx, [white_colour]
-    call XSetForeground
+    mov rdi, [paddle_x]
+    mov rsi, [paddle_y]
+    mov rdx, [paddle_width]
+    mov rcx, [paddle_width]
+    call draw_rectangle
 
-    ; draw paddle
-    mov rdi, [display]
-    mov rsi, [window]
-    mov rdx, [gc]
-    mov rcx, [paddle_x]
-    mov r8, [paddle_y]
-    mov r9, [paddle_width]
-    mov rax, [paddle_height]
-    push rax
-    call XFillRectangle
-    add rsp, 0x8
-
-    ; ensure all draw commands are flushed
-    mov rdi, [display]
-    call XFlush
-
-    ; sync commands with server
-    mov rdi, [display]
-    mov rsi, 0
-    call XSync
-
+    call render_end
+    
     ; get end frame time
     call get_time
-    
+
     ; see if we have spent less than 30ms in this frame
     mov rbx, [frame_time]
     sub rax, rbx
@@ -263,14 +164,6 @@ main_loop_end:
     call exit
 
 section .data
-    display: dq 0x0
-    screen_number: dq 0x0
-    black_colour: dq 0x0
-    white_colour: dq 0x0
-    default_root_window: dq 0x0
-    window: dq 0x0
-    gc: dq 0x0
-    event: resb 0xc0
     paddle_width: dq 0xc8
     paddle_height: dq 0x14
     paddle_x: dq 0x12c
@@ -283,6 +176,3 @@ section .rodata
     hello_world: db "hello world", 0xa, 0x0
     goodbye: db "goodbye", 0xa, 0x0
     sleep_for: db "sleep_for: ", 0x0
-    x_open_display_failed: db "XOpenDisplay failed", 0xa, 0x0
-    x_select_input_failed: db "XSelectInput failed", 0xa, 0x0
-    x_set_foreground_failed: db "XSetForeground failed", 0xa, 0x0
