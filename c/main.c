cf467735db6cfd5c0d26e6163669c4df3faeb6ac
iris dev <iris-engine-dev@gmail.com>
2022-11-05 18:26:14 +0000

    Integrate changes

--- c/main.c
+++ c/main.c
@@ -4,11 +4,75 @@
 //                 https://www.boost.org/LICENSE_1_0.txt)                     //
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+#include "SDL.h"
+#include "c_key_event.h"
+#include "c_rectangle.h"
+#include "c_window.h"
 
 int main()
 {
     printf("hello world\n");
+
+    // create window
+    C_Window *window;
+    if (c_window_create(&window) != C_SUCCESS)
+    {
+        printf("failed to create window\n");
+        exit(1);
+    }
+
+    C_KeyEvent event;
+    bool running = true;
+
+    while (running)
+    {
+        // process all events
+        for (;;)
+        {
+            C_Result event_result = c_window_get_event(window, &event);
+            if (event_result == C_SUCCESS)
+            {
+                if ((event.key_state == C_KEYSTATE_DOWN) && (event.key == C_KEY_Q))
+                {
+                    running = false;
+                }
+            }
+            else if (event_result == C_NO_MORE_EVENTS)
+            {
+                break;
+            }
+            else
+            {
+                printf("error getting event\n");
+                exit(1);
+            }
+        }
+
+        // render our scene
+
+        if (c_window_pre_render(window) != C_SUCCESS)
+        {
+            printf("pre render failed\n");
+            exit(1);
+        }
+
+        C_Rectangle paddle = c_rectangle_create_xy(300.0f, 780.0f, 300.0f, 20.0f);
+        if (c_window_draw_rectangle(window, &paddle, 0xff, 0xff, 0xff) != C_SUCCESS)
+        {
+            printf("failed to render\n");
+            exit(1);
+        }
+
+        c_window_post_render(window);
+    }
+
+    c_window_destroy(window);
+
+    printf("goodbye\n");
 
     return 0;
 }
