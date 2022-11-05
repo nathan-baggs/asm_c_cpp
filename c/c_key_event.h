a56a6d7bceee4196c04c2862ab75d8dd1866bdc8
iris dev <iris-engine-dev@gmail.com>
2022-11-05 18:11:54 +0000

    Add KeyEvent

---
+++ c/c_key_event.h
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+//         Distributed under the Boost Software License, Version 1.0.         //
+//            (See accompanying file LICENSE or copy at                       //
+//                 https://www.boost.org/LICENSE_1_0.txt)                     //
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+/**
+ * Enumeration of possible key states.
+ */
+typedef enum C_KeyState
+{
+    C_KEYSTATE_DOWN,
+    C_KEYSTATE_UP
+} C_KeyState;
+
+/**
+ * Enumeration of possible input keys (maybe incomplete).
+ */
+typedef enum C_Key
+{
+    C_KEY_Q
+} C_Key;
+
+/**
+ * Struct encapsulating the data for a key press event.
+ */
+typedef struct C_KeyEvent
+{
+    C_KeyState key_state;
+    C_Key key;
+} C_KeyEvent;
